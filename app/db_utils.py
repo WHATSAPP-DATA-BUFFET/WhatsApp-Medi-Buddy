3387968019e751b0a882baf7dc44ff1617900d42
CVHS <ponnusamy.k@valuehealthsol.com>
2024-06-29 15:27:31 +0530

    Update db_utils.py

--- app/db_utils.py
+++ app/db_utils.py
@@ -1,56 +1,35 @@
 import os
 from dotenv import load_dotenv
 from langchain_experimental.sql import SQLDatabaseSequentialChain
-
 from langchain_community.utilities.sql_database import SQLDatabase
-
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
-
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
-
 import psycopg2
-# session = requests.Session()
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 from langchain_community.utilities.sql_database import SQLDatabase
-# from langchain.sql_database import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseSequentialChain
-
 from langchain.prompts.prompt import PromptTemplate
 from langchain.output_parsers.list import CommaSeparatedListOutputParser
-
 from mistralai.client import MistralClient
-
 from langchain_mistralai.chat_models import ChatMistralAI
-
-
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.messages import SystemMessage
-
 from langchain_core.messages import HumanMessage
 from fastapi import HTTPException
 from langchain_core.output_parsers import JsonOutputParser
 
 base_dir = os.path.abspath(__file__)
 
-
 load_dotenv()
 
 schema_name = os.environ.get("DB_SCHEMA_NAME")
 db_connection = os.environ.get("db_connection")
-# print("db_connection---->",db_connection)
 password = os.environ.get('DB_PASSWORD')
 
-
-
 uft_api_key=os.getenv("FT_MISTRAL_API_KEY")
-
-# llm = ChatMistralAI(model="mistral-large-latest",api_key=uft_api_key)
-
-
-
 
 def model_v2_hf():
     repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
@@ -65,17 +44,12 @@ def model_v2_hf():
     return llm
 llm=model_v2_hf()
 
-
-
 def get_db_connection():
     print("get_db_connection-->" )
-    db_name = os.environ.get("DB_NAME") 
-    
+    db_name = os.environ.get("DB_NAME")
     db_user = os.environ.get("DB_USER")
     db_password = os.environ.get("DB_PASSWORD")
     db_host = os.environ.get("DB_HOST")
-
-    
     db_port = os.environ.get("DB_PORT")
     
     connection_string = f"dbname={db_name} user={db_user} password={db_password} host={db_host} port={db_port}"
@@ -85,10 +59,7 @@ def get_db_connection():
     print("db connection successfull!!!...")
     return conn, cursor
 
-
 connection, cursor = get_db_connection()
-
-       
 
 def db_connection_close(connection, cursor):
     try:
@@ -98,33 +69,19 @@ def db_connection_close(connection, cursor):
     except Exception as error:
         return {"response": f"{error}"}
     
-
 db_connection_close(connection, cursor)
 
 connection_string = db_connection % quote_plus(password)
 connection, cursor =  get_db_connection()
 
-
-
 def get_data(question):
     print("In get_data----------->")
-
- 
-
-
     engine = create_engine(connection_string)
-
-    
     db = SQLDatabase(engine,include_tables=["coverage_benefit"],schema = 'hackathon')
-
-
     PROMPT_SUFFIX = """Only use the following tables:
         {table_info} table used - coverage_benefit
 
         Question: {input}"""
-
-
-
     _DEFAULT_TEMPLATE = f"""You are an expert in writing sql queries.
 
         your task is to understand the user question and generate the sql query for getting the response from database(postgresql), you have to retreive response from 'coverage_benefit' table.
@@ -145,19 +102,23 @@ def get_data(question):
 
         if the user question contains 'secondary copay/copay balance': Data should be retrieved from the 'secondary_copay_usd' column *case sensitive.
 
-        
+        if the user question contains 'date of birth/DOB/birth date':Responses should be fetched from the 'date_of_birth' column *case sensitive in YYYY/MM/DD format.
+
         Your system should accurately interpret user queries and retrieve the corresponding information from the database. Ensure that the responses provided by the system are relevant and precise, enhancing the overall usability and efficiency of the conversational AI interface.
         
         Here is the question:{question}
         
         note: 
-            -Do not give the explanation about the query,just give answer retrieved from the database alone in  final response.
+        - Give a single 'SELECT' query  with 'date_of_birth' condition only.
 
-            -Do not give any other extra informations along with the Answer ,just give the Answer alone.
+        - Do not give any explanation about the generated query 'SQLQuery', just give query alone in response. 
 
-                
-            note : use schema name 'hackathon' in your query 
+        - Do not give any other extra informations along with the 'SQLResult' ,just give the Answer alone.
 
+        - SQLResult can either be 'qualified', 'unqualified', 'contacted', 'open'
+
+        note : use schema name 'hackathon' in your query 
+        
         your helpful response below:
         """
 
@@ -166,27 +127,17 @@ def get_data(question):
     template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,)
 
     db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db,query_prompt=PROMPT,verbose=False)
-    # db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db)
     response = db_chain.run(question)
-    # print(response)
     return response 
-
 
 def get_data_pa_status(question):
     print("In get_data_pa_status----------->")
-
     engine = create_engine(connection_string)
-
-    
     db = SQLDatabase(engine,include_tables=["care_prior_authorization"],schema = 'hackathon')
-
-
     PROMPT_SUFFIX = """Only use the following tables:
         {table_info} table used - care_prior_authorization
 
         Question: {input}"""
-    
-
     _DEFAULT_TEMPLATE = f"""You are an expert in writing sql queries.
 
         your task is to understand the user question and generate the sql query for getting the response from database(postgresql), you have to retreive response from 'care_prior_authorization' table.
@@ -201,7 +152,9 @@ def get_data_pa_status(question):
 
         if the user question contains 'Last Name': Information should be retrieved from the 'last_name' column *case sensitive.
 
-        if the user question contains 'email':Responses should be fetched from the 'email' column *case sensitive. 
+        if the user question contains 'email':Responses should be fetched from the 'email' column *case sensitive.
+
+        if the user question contains 'date of birth/DOB/birth date':Responses should be fetched from the 'date_of_birth' column *case sensitive in YYYY/MM/DD format. 
 
         if the user question contains 'pa status': Responses should be fetched from the  'status' column *case sensitive.    
         
@@ -211,46 +164,33 @@ def get_data_pa_status(question):
         Here is the question:{question}
 
         note: 
-            -Do not give the explanation about the query,just give answer retrieved from the database alone in final response.
+        - Give a single 'SELECT' query  with 'date_of_birth' condition only.
 
-            -Do not give any other extra informations along with the Answer ,just give the Answer alone.
+        - Do not give any explanation about the generated query 'SQLQuery', just give query alone in response. 
 
-                
-            note : use schema name 'hackathon' in your query 
+        - Do not give any other extra informations along with the 'SQLResult' ,just give the Answer alone.
 
+        - SQLResult can either be 'qualified', 'unqualified', 'contacted', 'open'
+
+        note : use schema name 'hackathon' in your query 
+        
         your helpful response below:
         """
-
-
-
     PROMPT = PromptTemplate(
     input_variables=["input", "table_info", "top_k"],
     template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,)
-
     db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db,query_prompt=PROMPT,verbose=False)
-    # db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db)
     response = db_chain.run(question)
-    # print(response)
     return response 
-
 
 def get_data_insurance(question):
     print("In get_data_insurance----------->")
-
     engine = create_engine(connection_string)
-
-    
     db = SQLDatabase(engine,include_tables=["insurance"],schema = 'hackathon')
-
-
-
     PROMPT_SUFFIX = """Only use the following tables:
         {table_info} table used - insurance
 
         Question: {input}"""
-
-    
-
     _DEFAULT_TEMPLATE = f"""You are an expert in writing sql queries.
 
         your task is to understand the user question and generate the sql query for getting the response from database(postgresql), you have to retreive response from 'insurance' table.
@@ -273,7 +213,7 @@ def get_data_insurance(question):
 
         if the user question contains 'insurance id/insurance number': Responses should be fetched from the  'insurance_id' column *case sensitive.
 
-        if the user question contains 'date of birth/2023-03-17':Responses should be fetched from the 'date_of_birth' column *case sensitive.
+        if the user question contains 'date of birth/DOB/birth date':Responses should be fetched from the 'date_of_birth' column *case sensitive in YYYY/MM/DD format.
 
         if the user question contains 'coverage details/insurance coverage':Responses should be fetched from the 'insurance_coverage' column *case sensitive.
 
@@ -283,45 +223,35 @@ def get_data_insurance(question):
         Here is the question:{question}
 
         note: 
-            -Do not give the explanation about the query,just give answer retrieved from the database alone in  final response.
+        - Give a single 'SELECT' query  with 'date_of_birth' condition only.
 
-            -Do not give any other extra informations along with the Answer ,just give the Answer alone.
+        - Do not give any explanation about the generated query 'SQLQuery', just give query alone in response. 
 
-                
-            note : use schema name 'hackathon' in your query 
+        - Do not give any other extra informations along with the 'SQLResult' ,just give the Answer alone.
 
+        - SQLResult can either be 'qualified', 'unqualified', 'contacted', 'open'
+
+        note : use schema name 'hackathon' in your query 
+        
         your helpful response below:
         """
-
-
-
+    
     PROMPT = PromptTemplate(
     input_variables=["input", "table_info", "top_k"],
     template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,)
-
     db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db,query_prompt=PROMPT,verbose=False)
-    # db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db)
     response = db_chain.run(question)
-    # print(response)
     return response 
-
 
 def get_data_case(question):
     print("In get_data_case----------->")
-
     engine = create_engine(connection_string)
-
-    #
     db = SQLDatabase(engine,include_tables=["report_case"],schema = 'hackathon')
-
 
     PROMPT_SUFFIX = """Only use the following tables:
         {table_info} table used - report_case
 
         Question: {input}"""
-
-
-
     _DEFAULT_TEMPLATE = f"""You are an expert in writing sql queries.
 
         your task is to understand the user question and generate the sql query for getting the response from database(postgresql), you have to retreive response from 'report_case' table.
@@ -336,7 +266,7 @@ def get_data_case(question):
 
         if the user question contains 'case id/case number': Information should be retrieved from the 'case_id' column *case sensitive.
 
-        if the user question contains 'date of birth/2023-03-17':Responses should be fetched from the 'date_of_birth' column *case sensitive.
+        if the user question contains 'date of birth/DOB/birth date':Responses should be fetched from the 'date_of_birth' column *case sensitive in YYYY/MM/DD format.
 
         if the user question contains 'phone number':Responses should be fetched from the 'phone_number' column *case sensitive. 
         
@@ -351,50 +281,36 @@ def get_data_case(question):
 
 
         note: 
-            -Do not give the explanation about the query,just give answer retrieved from the database alone in  final response.
+        - Give a single 'SELECT' query  with 'date_of_birth' condition only.
 
-            -Do not give any other extra informations along with the Answer ,just give the Answer alone.
+        - Do not give any explanation about the generated query 'SQLQuery', just give query alone in response. 
 
-                
-            note : use schema name 'hackathon' in your query 
+        - Do not give any other extra informations along with the 'SQLResult' ,just give the Answer alone.
 
+        - SQLResult can either be 'qualified', 'unqualified', 'contacted', 'open'
+
+        note : use schema name 'hackathon' in your query 
+        
         your helpful response below:
         """
-
-
-
 
     PROMPT = PromptTemplate(
     input_variables=["input", "table_info", "top_k"],
     template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,)
-
     db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db,query_prompt=PROMPT,verbose=False)
-    # db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db)
     response = db_chain.run(question)
-    # print(response)
     return response 
 
 def get_data_lead(question):
     print("In get_data_lead----------->")
-
     engine = create_engine(connection_string)
-
     print("engine--->",engine)
-
-    
     db = SQLDatabase(engine,include_tables=["lead_creation"],schema = 'hackathon')
-
-    print("db---->",db)
-
 
     PROMPT_SUFFIX = """Only use the following tables:
         {table_info} table used - lead_creation
 
         Question: {input}"""
-
-    
-
-
     _DEFAULT_TEMPLATE = f"""You are an expert in writing sql queries.
 
         your task is to understand the user question and generate the sql query for getting the response from database(postgresql), you have to retreive response from 'lead_creation' table.
@@ -411,7 +327,7 @@ def get_data_lead(question):
 
         if the user question contains 'email':Responses should be fetched from the 'email' column *case sensitive. 
 
-        if the user question contains 'date of birth':Responses should be fetched from the 'date_of_birth' column *case sensitive.
+        if the user question contains 'date of birth/DOB/birth date':Responses should be fetched from the 'date_of_birth' column *case sensitive in YYYY/MM/DD format.
 
         if the user question contains 'phone number':Responses should be fetched from the 'phone_number' column *case sensitive. 
 
@@ -433,26 +349,24 @@ def get_data_lead(question):
         Here is the question:{question}
 
         note: 
-            -Do not give the explanation about the query,just give answer retrieved from the database alone in  final response.
+        - Give a single 'SELECT' query  with 'date_of_birth' condition only.
 
-            -Do not give any other extra informations along with the Answer ,just give the Answer alone.
+        - Do not give any explanation about the generated query 'SQLQuery', just give query alone in response. 
 
-                
-            note : use schema name 'hackathon' in your query 
+        - Do not give any other extra informations along with the 'SQLResult' ,just give the Answer alone.
 
+        - SQLResult can either be 'qualified', 'unqualified', 'contacted', 'open'
+
+        note : use schema name 'hackathon' in your query 
+        
         your helpful response below:
         """
-
-
-
+    
     PROMPT = PromptTemplate(
     input_variables=["input", "table_info", "top_k"],
     template=_DEFAULT_TEMPLATE + PROMPT_SUFFIX,)
-
     db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db,query_prompt=PROMPT,verbose=False)
-    # db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm,db=db)
     response = db_chain.run(question)
-    # print(response)
     return response 
 
 # question="what is the lead status for this date of birth '2023-01-15'?"
